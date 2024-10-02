6ecc1a9a12230a984c95821dbf1dd7cebf6954ae
Nevezxy <duhneves16@gmail.com>
2024-10-02 03:15:39 -0300

    commit

--- escola/urls.py
+++ escola/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
 from escola.views import painel
+from .views import (
+    listar_professores,
+    detallhe_professor,
+    excluir_professor,
+    criar_professor,
+    editar_professor
+)
 
 urlpatterns = [
-    path('', painel, name='painel'),
+    path('', listar_professores, name='listar_professores'),
+    path('criar/', criar_professor, name='criar_prfessor'),
+    path('editar/<int:pk>/', editar_professor, name='editar_prfessor'),
+    path('detalhe/<int:pk>/', detallhe_professor, name='detalhe_professor'),
+    path('excluir/<int:pk>/', excluir_professor, name='excluir_prfessor'),
 ]
+
     
--- escola/views.py
+++ escola/views.py
@@ -3,17 +3,19 @@ from django.shortcuts import render
 
 from escola.forms import ProfessorForm
 from .models import Professor
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def painel(request):
     pass
 
-
+@login_required
 def listar_professores(request):
     professores = Professor.objects.all().order_by('nome')
     return render(request, 'professores/index.html', context={'professores': professores})
 
+@login_required
 def criar_professor(request):
     if request.method == "POST":
         professor_form = ProfessorForm(request.POST)
@@ -25,18 +27,21 @@ def criar_professor(request):
         else:
             messages.error(request, professor_form.errors)
 
+@login_required
 def excluir_professor(request, pk):
     professor = Professor.objects.get(id = pk)
     professor.delete()
 
     return listar_professores(request)
 
+@login_required
 def detallhe_professor(request, pk):
     professor = Professor.objects.get(id=pk)
     form = ProfessorForm(instance=professor)
 
     return render(request, 'professor/detalhe.html', context={'professor': professor, 'form': form})
 
+@login_required
 def editar_professor(request, pk):
     professor = Professor.objects.get(id=pk)
     if request.method == 'POST':
